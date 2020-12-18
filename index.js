60c0d065dcef2f107d3823a65e85979c907fa218
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 17:49:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(arr, call) {
+  arr.forEach(call);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely() {
+
+}
